82104d4dab770d4b0fa99855f4ee109bb76b1955
joshliford <j_liford1@yahoo.com>
2025-07-03 13:56:37 -0500

    Debugged code and added Explicit Type Conversion for each snippet. Also added 3 additional code snippets (Implicit, Implicit prior to Explicit, and Explicit Type Conversion)

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,27 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = Number("5") - 2; // This is already correct for Implicit Type Conversion, but adding the Number() method for Explicit Type Conversion improves clarity
+console.log("The result is: " + result); 
 
 let isValid = Boolean("false");
 if (isValid) {
-    console.log("This is valid!");
+    console.log(Boolean("This is valid!")); // Updated console.log to include the Boolean() method for Explicit Type Conversion - now evaluating to 'true'
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5; // Updated age to include the Number() method for Explicit Type Conversion - correcting string concatenation to addition (25 + 5)
 console.log("Total Age: " + totalAge);
+
+// Part 2:
+
+let total = "10" * 2;
+console.log("The total is: " + total); // Example of Implicit Type Conversion
+
+let number = null;
+let implicitValue = number;
+console.log("The value of null is: " + implicitValue); // Example of Implicit Type Conversion prior to implementing Explicit Type Conversion
+
+let value = null;
+let numberValue = Number(value);
+console.log("The value of null is: " + numberValue); // Example of Explicit Type Conversion - using null
